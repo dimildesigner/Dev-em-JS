8fe94086e3c3c9a3b879dbe0ba8b75a009859dfb
Edimilson Teixeira <97994730+dimildesigner@users.noreply.github.com>
2025-08-01 18:00:37 -0300

    Add files via upload

---
+++ aula06/variaveis.js
@@ -0,0 +1,33 @@
+// Comentário de uma linha
+
+/*
+Comentário que pula linha
+*/
+
+// Estrutura de dados em JavaScript
+// "var" não é mais usado por causa de vasamento
+var numero1 = 10
+
+// "let" é variável
+let numero2 = 8
+
+// "const" é o valor fixo
+const numero3 = 30
+
+// o sinal de "=" não quer dizer igualdade e sim "recebe"
+
+
+// TIPOS DE DADOS
+
+// real - exemplos: 1.2  3.0   4.5  -  dinheiro, peso, altura...
+// boleano - exemplos: true false 0/1 - condições
+// inteiro - exemplos: -1  0  1  - altura, ano, nº de filhos...
+// texto - exemplos: 'Isso é um texto'  "Isso tb é um texto"  - msg, label...
+
+// Exemplos
+let idade = 25
+let peso = 65.12
+let x = 'Isso é um texto'
+
+console.log(idade)
+
